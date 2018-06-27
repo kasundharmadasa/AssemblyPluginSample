43c4c6c63202db7045c5b8fb72eb937eda420277
kasundharmadasa <kasunpdh@gmail.com>
2018-06-27 20:16:47 +0530

    intial commit

---
+++ src/main/java/com/sample/Main.java
@@ -0,0 +1,29 @@
+package com.sample;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Properties prop = new Properties();
+        try (InputStream input = new FileInputStream("conf/config.properties")) {
+
+            // load a properties file
+            prop.load(input);
+            String name = prop.getProperty("name");
+
+            if (StringUtils.isEmpty(name)) {
+                System.out.println(name);
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
